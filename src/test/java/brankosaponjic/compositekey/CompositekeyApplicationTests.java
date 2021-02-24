a24b4a623448dce89d4bb7891d9a8022d3692079
Branko <brankosaponjic76@gmail.com>
2021-02-24 23:12:48 +0100

    Created test customer method. Closes #3

--- src/test/java/brankosaponjic/compositekey/CompositekeyApplicationTests.java
+++ src/test/java/brankosaponjic/compositekey/CompositekeyApplicationTests.java
@@ -1,13 +1,24 @@
 package brankosaponjic.compositekey;
 
+import brankosaponjic.compositekey.entities.Customer;
+import brankosaponjic.compositekey.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompositekeyApplicationTests {
 
+    @Autowired
+    CustomerRepository repository;
+
     @Test
-    void contextLoads() {
+    void testCreateCustomer() {
+        Customer customer = new Customer();
+        customer.setId(1234);
+        customer.setEmail("johndoe@gmail.com");
+        customer.setName("John Doe");
+        repository.save(customer);
     }
 
 }
